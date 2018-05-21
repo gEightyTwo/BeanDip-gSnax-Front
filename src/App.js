e4f8849eeaef130745f0f1d0f89ee99e2cb96340
Claudia Ligidakis <30705905+claudialigidakis@users.noreply.github.com>
2018-05-21 13:42:29 -0700

    added footer/avbar templates

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
 
 import Footer from './components/Footer'
+import Navbar from './components/Navbar'
 
 class App extends React.Component {
   render() {
     return (
       <div>
+        <Navbar />
         <Footer />
       </div>
     );
   }
 }
-
 export default App
