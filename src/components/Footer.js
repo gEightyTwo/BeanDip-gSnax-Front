f225f8a3261f4c290d99ae7e5581e06d915d99ff
Claudia Ligidakis <30705905+claudialigidakis@users.noreply.github.com>
2018-05-22 14:15:29 -0700

    updated the navbar

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,51 +1,85 @@
 import React from 'react';
 
-export default (props) => (
-  <footer className="page-footer font-small mdb-color pt-4 mt-4">
-
+export default(props) => (<footer>
+  <div className="footer page-footer font-small">
     <div className="container text-center text-md-left">
+      <div className="row text-center text-md-left mt-3 pb-3">
+        <div className="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+          <h6 className="text-uppercase mb-4 font-weight-bold">BeanDip</h6>
+          <p>Here you can explore all the different snacks and compare how they would be as a Dip or even a Dipping Tool.</p>
+        </div>
+        <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+          <h6 className="text-uppercase mb-4 font-weight-bold">Products</h6>
+          <p>
+            <a href="#!">Snacks</a>
+          </p>
+          <p>
+            <a href="#!">Dips</a>
+          </p>
+          <p>
+            <a href="#!">Judgement</a>
+          </p>
+        </div>
+        <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
+          <h6 className="text-uppercase mb-4 font-weight-bold">Useful links</h6>
+          <p>
+            <a href="#!">Your Account</a>
+          </p>
+          <p>
+            <a href="#!">Become an Affiliate</a>
+          </p>
+          <p>
+            <a href="#!">Shipping Rates</a>
+          </p>
+          <p>
+            <a href="#!">Help</a>
+          </p>
+        </div>
 
-  <div className="row text-center text-md-left mt-3 pb-3">
+        <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
+          <h6 className="text-uppercase mb-4 font-weight-bold">Contact</h6>
+          <p>
+            <i className="fa fa-home mr-3"></i>Galvanize</p>
+          <p>
+            <i className="fa fa-envelope mr-3"></i>
+            info@gmail.com</p>
+          <p>
+            <i className="fa fa-phone mr-3"></i>
+            + 01 234 567 88</p>
+          <p>
+            <i className="fa fa-print mr-3"></i>
+            + 01 234 567 89</p>
+        </div>
 
-    <div className="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
-      <h6 className="text-uppercase mb-4 font-weight-bold">BeanDip</h6>
-      <p>Here you can explore all the different snacks and compare how they would be as a Dip or even a Dipping Tool.</p>
-    </div>
-    <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
-      <h6 className="text-uppercase mb-4 font-weight-bold">Products</h6>
-      <p><a href="#!">Snacks</a></p>
-      <p><a href="#!">Dips</a></p>
-      <p><a href="#!">Judgement</a></p>
-    </div>
-    <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
-      <h6 className="text-uppercase mb-4 font-weight-bold">Useful links</h6>
-      <p><a href="#!">Your Account</a></p>
-      <p><a href="#!">Become an Affiliate</a></p>
-      <p><a href="#!">Shipping Rates</a></p>
-      <p><a href="#!">Help</a></p>
-    </div>
-
-    <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
-      <h6 className="text-uppercase mb-4 font-weight-bold">Contact</h6>
-      <p><i className="fa fa-home mr-3"></i>Galvanize</p>
-      <p><i className="fa fa-envelope mr-3"></i> info@gmail.com</p>
-      <p><i className="fa fa-phone mr-3"></i> + 01 234 567 88</p>
-      <p><i className="fa fa-print mr-3"></i> + 01 234 567 89</p>
-    </div>
-
-  </div>
-  <div className="row d-flex align-items-center">
-    <div className="col-md-4 col-lg-4 ml-lg-0">
-      <div className="text-center text-md-right">
-        <ul className="list-unstyled list-inline">
-          <li className="list-inline-item"><a className="btn-floating btn-sm rgba-white-slight mx-1"><i className="fa fa-facebook"></i></a></li>
-          <li className="list-inline-item"><a className="btn-floating btn-sm rgba-white-slight mx-1"><i className="fa fa-twitter"></i></a></li>
-          <li className="list-inline-item"><a className="btn-floating btn-sm rgba-white-slight mx-1"><i className="fa fa-google-plus"></i></a></li>
-          <li className="list-inline-item"><a className="btn-floating btn-sm rgba-white-slight mx-1"><i className="fa fa-linkedin"></i></a></li>
-        </ul>
+      </div>
+      <div className="row d-flex align-items-center">
+        <div className="col-md-4 col-lg-4 ml-lg-0">
+          <div className="text-center text-md-right">
+            <ul className="list-unstyled list-inline">
+              <li className="list-inline-item">
+                <a className="btn-floating btn-sm rgba-white-slight mx-1">
+                  <i className="fa fa-facebook"></i>
+                </a>
+              </li>
+              <li className="list-inline-item">
+                <a className="btn-floating btn-sm rgba-white-slight mx-1">
+                  <i className="fa fa-twitter"></i>
+                </a>
+              </li>
+              <li className="list-inline-item">
+                <a className="btn-floating btn-sm rgba-white-slight mx-1">
+                  <i className="fa fa-google-plus"></i>
+                </a>
+              </li>
+              <li className="list-inline-item">
+                <a className="btn-floating btn-sm rgba-white-slight mx-1">
+                  <i className="fa fa-linkedin"></i>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
   </div>
-</div>
-</footer>
-  )
+</footer>)
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,32 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+// import {Link} from 'react-router-dom'
 
 export default(props) => (
-  <div className="nav">
+  <header>
+  <div className="jumbotron jumbotron-billboard">
+    <div className="img"></div>
+    <div className="nav">
       <div className='navbar'>
         <div className='navlogo'>
-          <h3>BeanDip</h3>
+          <h2>BeanDip</h2>
         </div>
         <div className='navlinks'>
-          <a className="btn btn-sm btn-outline-secondary" to="/">Sign up</a>
-          <a className="btn btn-sm btn-outline-secondary" to="/login">Sign in</a>
-          <a href="#" className='hidden'>Log Out</a>
+          <btn className="btn btn-outline-secondary" to="/login">Sign in</btn>
+          <button className='hidden'>Log Out</button>
         </div>
       </div>
     </div>
-)
+    <div className="container">
+      <div className="row">
+        <div className="col-lg-12">
+          <h2 text="text">SnackTime</h2>
+          <p>
+            Find out what other people are saying about your favorite snacks!
+          </p>
+          <a href="" className="btn btn-success btn-lg">Sign Up</a>
+        </div>
+      </div>
+    </div>
+  </div>
+
+</header>)
